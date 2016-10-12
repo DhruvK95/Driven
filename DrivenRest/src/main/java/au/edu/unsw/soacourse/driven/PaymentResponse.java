ec9503b9407ed7a849d5f08e13df5bca6a984b25
Dhruv <dhruvk995@gmail.com>
2016-10-12 19:07:18 +1100

    PaymentResponse class added

---
+++ DrivenRest/src/main/java/au/edu/unsw/soacourse/driven/PaymentResponse.java
@@ -0,0 +1,30 @@
+package au.edu.unsw.soacourse.driven;
+
+/**
+ * Created by Dhruv on 12/10/2016. DrivenRest
+ */
+public class PaymentResponse {
+    Payment payment;
+    String Link;
+
+    public PaymentResponse(Payment payment, String link) {
+        this.payment = payment;
+        Link = link;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+}
