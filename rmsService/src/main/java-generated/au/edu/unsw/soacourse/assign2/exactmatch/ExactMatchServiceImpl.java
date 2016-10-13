30a567ba3260dc46a4991aeebee1b07b11b8a5b2
leonaugustine <z5022751@student.unsw.edu.au>
2016-10-13 13:16:13 +1100

    Faults added for all casses in exact, carring over to similair

--- rmsService/src/main/java-generated/au/edu/unsw/soacourse/assign2/exactmatch/ExactMatchServiceImpl.java
+++ rmsService/src/main/java-generated/au/edu/unsw/soacourse/assign2/exactmatch/ExactMatchServiceImpl.java
@@ -21,32 +21,54 @@ public class ExactMatchServiceImpl implements ExactMatchService{
 		ValidateExactMatchResponse res  = factory.createValidateExactMatchResponse();		
 		
 		System.out.println("DriversLicenseServiceImpl ==" +parameters.address);
-		if(parameters.address.equals("")){
-			System.out.print("HERE");
-			ServiceFaultType simAddFaultMessage = new ServiceFaultType();
-            simAddFaultMessage.setErrtext("null");
-            simAddFaultMessage.setErrcode("no address");
-			throw new NullEntryFaultMsg("null",simAddFaultMessage);
-			
-		}
-		if(parameters.address.equals("home")){
-			res.message="\n "
-					+ "Exact Address Found \n";
-			res.messageStatus="Output Closed"; 
-		}else{
-			res.message=parameters.address;
-			res.messageStatus="no"; 			
-		}
+
+		res.message=parameters.address;
+		res.messageStatus="no"; 			
+		int count = 0;
+		for(int i =0; i < parameters.address.length(); i++)
+		    if(parameters.address.charAt(i) == ',')
+		        count++;
+		
         String pattern = "(.*)?,(.*)?,(.*)?,(.*)?,(.*)?";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(parameters.address);
+        int groupCount = m.groupCount();
+        System.out.println("Number of group = " + groupCount);
+
         if (m.find()) {
             System.out.println("Found street number: " + m.group(1) );
             System.out.println("Found street name: " + m.group(2) );
             System.out.println("Found suburb name: " + m.group(3) );
             System.out.println("Found state: " + m.group(4) );
             System.out.println("Found postcode: " + m.group(5) );
+
         }
+        if (count>4){
+			res.message="Exessive Entry Fault";
+			res.messageStatus="No values entered for address"
+					+ "\n  Feild Entry Structure: StrNumber,StrName,SubName,StateName,"; 
+			return res;
+		}else if(count <4){
+			res.message="Insufficient Entry Fault";
+			res.messageStatus="No values entered for address"
+					+ "\n  Feild Entry Structure: StrNumber,StrName,SubName,StateName,"; 
+			return res;
+		}else if(parameters.address.equals("")){
+			res.message="Null Entry Fault";
+			res.messageStatus="No values entered for address"
+					+ "\n  Feild Entry Structure: StrNumber,StrName,SubName,StateName,"; 
+			return res;
+		
+		}else if(!(m.group(1).matches("[-+]?\\d*\\.?\\d+") && m.group(5).matches("[-+]?\\d*\\.?\\d+"))) {
+				res.message="Number Entry Fault";
+				res.messageStatus="Street Number OR Postcode is not a number";
+				return res;
+		}else if(!((m.group(2).matches("[a-zA-Z ]+")) && (m.group(3).matches("[a-zA-Z ]+")) & (m.group(4).matches("[a-zA-Z ]+")))){
+				res.message="Text Entry Fault";
+				res.messageStatus="Street Name OR Suburb Name OR State Name is not text ";
+				return res;
+		}
+
 		// Connect to DB to check for exact match
         Connection c = null;
         Statement stmt = null;
@@ -115,7 +137,7 @@ public class ExactMatchServiceImpl implements ExactMatchService{
        res.messageStatus="no"; 
 		
 		
-		return res;		
+       return res;		
 	}
 
 }
